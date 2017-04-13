95ddcd01c963e52823430f542563302044582004
Ray <izhangshuanglei@gmail.com>
2017-04-13 18:53:15 +0800

    sadf

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # coding: utf-8
-MYSQL_HOST = 'xxxxx'
+MYSQL_HOST = 'xxx sxx'
 MYSQL_DBNAME = 'wzxx8db'         #数据库名字，请修改
 MYSQL_USER = 'gotbmeadmin'             #数据库账号，请修改
 MYSQL_PORT = 3306               #数据库端口，在dbhelper中使用
